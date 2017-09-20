3aebd2fc388e0f38807839b6e7a192daba89148e
Vinay Goel <vinaygo@gmail.com>
2017-09-20 13:36:08 -0700

    install services

--- install-services.sh
+++ install-services.sh
@@ -14,7 +14,7 @@ fi
 if [ ! -d "$KIBANA_INSTALL_DIR" ]; then
   mkdir -p $KIBANA_INSTALL_DIR
   cd $KIBANA_INSTALL_DIR
-  curl -O https://artifacts.elastic.co/downloads/kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz
+  curl -O https://download.elastic.co/kibana/kibana/kibana-${KIBANA_VERSION}-linux-x64.tar.gz
   tar xfz kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz --no-same-owner
   mv kibana-${KIBANA_VERSION}-linux-x86_64 kibana-${KIBANA_VERSION}
   echo "server.port: ${KIBANA_PORT}" >> kibana-${KIBANA_VERSION}-linux-x86_64/config/kibana.yml
@@ -25,7 +25,7 @@ fi
 if [ ! -d "$ELASTICSEARCH_INSTALL_DIR" ]; then
   mkdir -p $ELASTICSEARCH_INSTALL_DIR
   cd $ELASTICSEARCH_INSTALL_DIR
-  curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTICSEARCH_VERSION}.tar.gz
+  curl -O https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${ELASTICSEARCH_VERSION}.tar.gz
   tar xfz elasticsearch-${ELASTICSEARCH_VERSION}.tar.gz --no-same-owner
   echo "path.data: /ars-elasticsearch-index-data" >> elasticsearch-${ELASTICSEARCH_VERSION}/config/elasticsearch.yml
   echo "http.port: ${ELASTICSEARCH_PORT}" >> elasticsearch-${ELASTICSEARCH_VERSION}/config/elasticsearch.yml
